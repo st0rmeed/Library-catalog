5f5965f2d4de8710cc741f966aaab42ac4c506f5
st0rmed <gleblazarev967@gmail.com>
2025-01-11 21:20:18 +0300

    add default picture and information about book

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
 import sys
 import sqlite3
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QPushButton, QVBoxLayout, QWidget, QLineEdit, \
-    QComboBox, QTableWidgetItem, QSizePolicy, QDialog, QLabel
+    QComboBox, QTableWidgetItem, QSizePolicy, QDialog, QLabel, QHBoxLayout, QFormLayout
 from PyQt6.QtGui import QPixmap
 from PyQt6 import uic
+
+
+class BookInfoDialog(QDialog):
+    def __init__(self, book_id):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        con = sqlite3.connect('books.sqlite')
+        cur = con.cursor()
+        result = cur.execute('''
+            SELECT 
+                title, author, year, genre, image 
+            FROM 
+                books 
+            WHERE id = ?''', (book_id,)).fetchone()
+        con.close()
+
+        if result:
+            title, author, year, genre, image_data = result
+
+            if image_data is not None and len(image_data) > 0:
+                pixmap = QPixmap()
+                pixmap.loadFromData(image_data)
+            else:
+                pixmap = QPixmap('default.jpg')
+
+            image_label = QLabel()
+            image_label.setPixmap(pixmap)
+            layout.addWidget(image_label)
+
+            form_layout = QFormLayout()
+            form_layout.addRow('Название:', QLabel(title))
+            form_layout.addRow('Автор:', QLabel(author))
+            form_layout.addRow('Год издания:', QLabel(str(year)))
+            form_layout.addRow('Жанр:', QLabel(genre))
+            layout.addLayout(form_layout)
+
+            self.setLayout(layout)
+        else:
+            self.close()
 
 
 class MainWindow(QMainWindow):
@@ -24,13 +64,13 @@ class MainWindow(QMainWindow):
         self.book_ids = []
 
     def search(self):
-        text = self.combo.currentText()
+        search_field = self.combo.currentText()
         search_text = f'%{self.edit.text()}%'
 
-        con = sqlite3.connect("books.sqlite")
+        con = sqlite3.connect('books.sqlite')
         cur = con.cursor()
 
-        if text == 'Автор':
+        if search_field == 'Автор':
             self.result = cur.execute('''
             SELECT 
                 id, title
@@ -68,29 +108,8 @@ class MainWindow(QMainWindow):
             self.table.setCellWidget(i, 0, button)
 
     def show_image(self, book_id):
-        con = sqlite3.connect('books.sqlite')
-        cur = con.cursor()
-
-        image_data = cur.execute('''
-        SELECT 
-            image 
-        FROM 
-            books 
-        WHERE id = ?''', (book_id,)).fetchone()
-
-        if image_data and image_data[0]:
-            pixmap = QPixmap()
-            pixmap.loadFromData(image_data[0])
-
-            image_dialog = QDialog(self)
-            image_label = QLabel()
-            image_label.setPixmap(pixmap)
-            layout = QVBoxLayout()
-            layout.addWidget(image_label)
-            image_dialog.setLayout(layout)
-            image_dialog.exec()
-
-        con.close()
+        dialog = BookInfoDialog(book_id)
+        dialog.exec()
 
 
 if __name__ == '__main__':
